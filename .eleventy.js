847fd9ff4f5314566b61f9bf61d6ac947e125b78
jisbruzzi <jose.sbru@gmail.com>
2021-12-14 22:29:32 -0300

    Photos

--- .eleventy.js
+++ .eleventy.js
@@ -54,6 +54,7 @@ module.exports = function(eleventyConfig) {
 
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("css");
 
   // Customize Markdown library and settings:
--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -24,5 +24,6 @@ module.exports = {
   variants: {},
   plugins: [
     require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
